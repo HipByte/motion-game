92161f08232a123a6658bfed66b9575f5a84a304
Laurent Sansonetti <lrz@hipbyte.com>
2015-02-23 18:10:16 +0100

    proper android initialization + make sure to “use” the JNI symbols from cocos2d that are expected to be looked dynamically by the JVM (since they are linked statically…)

--- src/init.cpp
+++ src/init.cpp
@@ -1,4 +1,7 @@
 #include "rubymotion.h"
+#if CC_TARGET_OS_ANDROID
+# include "platform/android/jni/JniHelper.h"
+#endif
 
 VALUE rb_mMC = Qnil;
 
@@ -6,6 +9,42 @@ extern "C"
 void
 Init_Fluency(void)
 {
+#if CC_TARGET_OS_ANDROID
+    assert(RubyVM::java_vm != NULL);
+    cocos2d::JniHelper::setJavaVM(RubyVM::java_vm);
+
+#define USE_SYMBOL(x) { extern void *x; void *_sym = x; }
+    USE_SYMBOL(Java_org_cocos2dx_lib_GameControllerAdapter_nativeControllerAxisEvent)
+    USE_SYMBOL(Java_org_cocos2dx_lib_GameControllerAdapter_nativeControllerButtonEvent)
+    USE_SYMBOL(Java_org_cocos2dx_lib_GameControllerAdapter_nativeControllerConnected)
+    USE_SYMBOL(Java_org_cocos2dx_lib_GameControllerAdapter_nativeControllerDisconnected)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxVideoHelper_nativeExecuteVideoCallback)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxWebViewHelper_didFailLoading)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxWebViewHelper_didFinishLoading)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxWebViewHelper_onJsCallback)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxWebViewHelper_shouldStartLoading)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxBitmap_nativeInitBitmapDC)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxActivity_getGLContextAttrs)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeInit)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeOnSurfaceChanged)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxAccelerometer_onSensorChanged)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxHelper_nativeSetApkPath)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxHelper_nativeSetContext)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxHelper_nativeSetEditTextDialogResult)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeDeleteBackward)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeGetContentText)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeInsertText)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeOnPause)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeOnResume)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeRender)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeKeyDown)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeTouchesBegin)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeTouchesCancel)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeTouchesEnd)
+    USE_SYMBOL(Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeTouchesMove)
+#undef USE_SYMBOL
+#endif
+
     rb_mMC = rb_define_module("MC");
 
 #define INIT_MODULE(x) \
