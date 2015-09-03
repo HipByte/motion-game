6e83301016df075e833b93a3f746b279b0d3f987
Laurent Sansonetti <lrz@hipbyte.com>
2015-09-03 15:39:28 +0200

    Label -> Text

--- template/motion-game/files/app/main_scene.rb
+++ template/motion-game/files/app/main_scene.rb
@@ -1,6 +1,6 @@
 class MainScene < MG::Scene
   def initialize
-    label = MG::Label.new("Hello World", "Arial", 96)
+    label = MG::Text.new("Hello World", "Arial", 96)
     label.anchor_point = [0, 0]
     add label
   end
