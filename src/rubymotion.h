0ea3de78ece6e2bf3ed3adabbdd8fe35def81130
Laurent Sansonetti <lrz@hipbyte.com>
2015-12-13 18:23:26 +0900

    remove dependency on rubymotion-ios headers

--- src/rubymotion.h
+++ src/rubymotion.h
@@ -6,21 +6,60 @@
 #if CC_TARGET_OS_IPHONE || CC_TARGET_OS_APPLETV
 
 #include <objc/runtime.h>
-#include "macruby_internal.h"
-#include "ruby/intern.h"
-#include "ruby/node.h"
-#include "ruby/io.h"
-#include "id.h"
-#include "vm.h"
 
+#define T_NATIVE 0x16
+#define VALUE		unsigned long
+#define SIGNED_VALUE	long
+#define Qfalse	0x0
+#define Qtrue	0x2
+#define Qnil	0x4
+
+long rb_ary_len(VALUE);
+VALUE rb_ary_elt(VALUE, long);
+#define RARRAY_LEN(a) (rb_ary_len((VALUE)a))
+#define RARRAY_AT(a,i) (rb_ary_elt((VALUE)a, (long)i))
+
+double rb_num2dbl(VALUE);
+VALUE rb_float_new(double);
+#define NUM2DBL(x) rb_num2dbl((VALUE)(x))
+#define DBL2NUM(x) rb_float_new(x)
+
+#define FIXNUM_FLAG	0x01
+#define IMMEDIATE_MASK	0x03
+#define FIXNUM_P(f) ((((SIGNED_VALUE)(f)) & IMMEDIATE_MASK) == FIXNUM_FLAG)
+long rb_num2long(VALUE);
+#define FIX2LONG(x) (x >> 2)
+#define NUM2LONG(x) (FIXNUM_P(x) ? FIX2LONG(x) : rb_num2long((VALUE)x))
+VALUE rb_uint2big(VALUE);
+#define LONG2FIX(i) ((VALUE)(((SIGNED_VALUE)(i))<<2 | FIXNUM_FLAG))
+#define FIXNUM_MAX (LONG_MAX>>2)
+#define POSFIXABLE(f) (((VALUE)f) < FIXNUM_MAX+1)
+#define INT2NUM(x) (POSFIXABLE(x) ? LONG2FIX(x) : rb_uint2big(x))
+#define LONG2NUM(x) INT2NUM(x)
+long rb_num2int(VALUE);
+#define FIX2INT(x) ((int)FIX2LONG(x))
+#define NUM2INT(x) (FIXNUM_P(x) ? FIX2INT(x) : rb_num2int((VALUE)x))
+
+const char *rb_str_cstr(VALUE);
+#define RSTRING_PTR(str) (rb_str_cstr((VALUE)str))
+
+VALUE rb_obj_is_kind_of(VALUE, VALUE);
+
+void rb_objc_define_method(VALUE klass, const char *name, void *imp,
+	const int arity);
 #define rb_define_method(klass, name, imp, arity) \
     rb_objc_define_method(klass, name, (void *)imp, arity)
 
+#define RB_GET_CLASS(obj) (VALUE)object_getClass((id)obj)
 #define rb_define_singleton_method(klass, name, imp, arity) \
     rb_define_method(RB_GET_CLASS(klass), name, imp, arity)
 
+VALUE rb_define_module(const char *);
+VALUE rb_define_module_under(VALUE, const char *);
+
 #define rb_selector(str) sel_registerName(str)
 
+VALUE rb_vm_call(VALUE self, SEL sel, int argc, const VALUE *argv);
 #define rb_send(rcv, sel, argc, argv) rb_vm_call(rcv, sel, argc, argv)
 
 struct rb_class_ptr {
@@ -45,13 +84,16 @@ rb_class_wrap_new(void *ptr, VALUE klass)
 
 #define rb_class_wrap_get_ptr(obj) \
     ((struct rb_class_ptr *)obj)->ptr
-    
+
+void *rb_objc_retain(void *addr);
 #define rb_retain(obj) \
     ({ \
 	VALUE _obj = (VALUE)obj; \
 	rb_objc_retain((void *)_obj); \
 	_obj; \
     })
+
+void *rb_objc_release(void *addr);
 #define rb_release(obj) \
     ({ \
 	VALUE _obj = (VALUE)obj; \
@@ -59,16 +101,17 @@ rb_class_wrap_new(void *ptr, VALUE klass)
 	_obj; \
     })
 
+void *rb_vm_current_block(void);
 #define rb_current_block() \
     ({ \
 	VALUE _b = (VALUE)rb_vm_current_block(); \
 	_b == 0 ? Qnil : _b; \
     })
 
-#define rb_block_call(block, argc, argv) \
-    rb_vm_block_eval((rb_vm_block_t *)block, argc, argv)
+VALUE rb_vm_block_eval(void *block, int argc, const VALUE *argv);
+#define rb_block_call(block, argc, argv) rb_vm_block_eval
 
-#define RSTRING_NEW(cstr) rb_str_new2(cstr)
+VALUE rb_define_class_under(VALUE, const char*, VALUE);
 
 #elif CC_TARGET_OS_ANDROID
 
@@ -107,7 +150,6 @@ rb_class_wrap_new(void *ptr, VALUE klass)
 	_local; \
     })
 
-#define RSTRING_NEW(cstr) rb_str_new2(cstr)
 #define RSTRING_PTR(str) rb_str_to_stdstring(str).c_str()
 
 #define rb_current_block() rb_vm_current_block_object()
@@ -115,6 +157,29 @@ rb_class_wrap_new(void *ptr, VALUE klass)
     rb_vm_block_dispatch(RB_BLOCK_PTR(block), argc, argv)
 
 #endif
+
+extern VALUE rb_cArray;
+extern VALUE rb_cSymbol;
+extern VALUE rb_cObject;
+extern VALUE rb_cInteger;
+extern VALUE rb_eArgError;
+extern VALUE rb_eRuntimeError;
+
+void rb_raise(VALUE exc, const char *fmt, ...) __attribute__ ((noreturn));
+const char *rb_sym2name(VALUE sym);
+int rb_scan_args(int, const VALUE*, const char*, ...);
+VALUE rb_ary_new(void);
+VALUE rb_ary_push(VALUE, VALUE);
+VALUE rb_name2sym(const char *);
+
+#define RTEST(obj) \
+    ({ \
+        VALUE _rtest_obj = (VALUE)obj; \
+        _rtest_obj != Qfalse && _rtest_obj != Qnil; \
+    })
+
+VALUE rb_str_new2(const char *);
+#define RSTRING_NEW(cstr) rb_str_new2(cstr)
 
 #if defined(__cplusplus)
 extern "C" {
