aa9f117c42e05119b31d014940541b7af7aa90ce
Benjamin Michotte <bmichotte@gmail.com>
2015-12-31 13:47:14 +0100

    MG::FileUtils

---
+++ src/file.cpp
@@ -0,0 +1,26 @@
+#include "rubymotion.h"
+
+/// @class FileUtils
+
+VALUE rb_cFileUtils = Qnil;
+
+/// @method #read
+/// Open a file and return its content
+/// @return [String] the content.
+
+static VALUE
+file_read(VALUE rcv, SEL sel, VALUE val)
+{
+	std::string filename = RSTRING_PTR(val);
+  auto content = cocos2d::FileUtils::getInstance()->getStringFromFile(filename);
+  return RSTRING_NEW(content.c_str());
+}
+
+extern "C"
+void
+Init_FileUtils(void)
+{
+    rb_cFileUtils = rb_define_class_under(rb_mMC, "FileUtils", rb_cObject);
+
+    rb_define_singleton_method(rb_cFileUtils, "read", file_read, 1);
+}
