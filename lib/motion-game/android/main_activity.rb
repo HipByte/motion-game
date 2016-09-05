42ddde925481693f9975b076f6862d81a0cca902
Watson <watson1978@gmail.com>
2016-09-06 02:16:41 +0900

    add same logic in previous commit for Android #46

--- lib/motion-game/android/main_activity.rb
+++ lib/motion-game/android/main_activity.rb
@@ -1,2 +1,15 @@
 class MainActivity < Org::Cocos2dx::Lib::Cocos2dxActivity
+  def onResume
+    super
+    director = MG::Director.shared
+    director.resume
+    director.start_animation
+  end
+
+  def onPause
+    super
+    director = MG::Director.shared
+    director.pause
+    director.stop_animation
+  end    
 end
