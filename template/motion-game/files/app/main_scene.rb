e23ea839605e230e4a586479dfff8809a7175d91
Watson <watson1978@gmail.com>
2016-12-22 22:37:55 +0900

    [#66] remove "MG::" in template

--- template/motion-game/files/app/main_scene.rb
+++ template/motion-game/files/app/main_scene.rb
@@ -1,7 +1,7 @@
 module MG
   class MainScene < Scene
     def initialize
-      label = MG::Text.new("Hello World", "Arial", 96)
+      label = Text.new("Hello World", "Arial", 96)
       label.anchor_point = [0, 0]
       add label
     end
