4892a029f37f890eec3d7bd74b59af3a5deb7c8f
Watson <watson1978@gmail.com>
2016-12-27 08:23:28 +0900

    [#45] configure design resolution size and content scale factor

--- samples/SpaceGame/app/application.rb
+++ samples/SpaceGame/app/application.rb
@@ -1,6 +1,9 @@
 class Application < MG::Application
   def start
     director = MG::Director.shared
+    size = director.size
+    director.glview.design_resolution_size(size.width, size.height, MG::ResolutionPolicy::SHOW_ALL)
+    director.content_scale_factor = 750.0 / size.height
     director.show_stats = true
     director.run(MG::MainScene.new)
   end
