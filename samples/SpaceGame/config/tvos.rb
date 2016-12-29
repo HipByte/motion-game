725c154ab7c3608d268e21394a293d2fa268d808
Watson <watson1978@gmail.com>
2016-12-29 18:48:23 +0900

    [sample] update with currently template files

--- samples/SpaceGame/config/tvos.rb
+++ samples/SpaceGame/config/tvos.rb
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+begin
+  require 'bundler'
+  Bundler.require(:default, :tvos)
+rescue LoadError
+end
+
 Motion::Project::App.setup do |app|
   # Use `rake tvos:config' to see complete project settings.
   app.name = 'SpaceGame'
